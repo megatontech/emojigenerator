8ee99b2f6a7ba379157b27398625d0647f3dd291
megatontech <techme@live.cn>
2016-11-22 15:23:35 +0800

    完成了图片加载清空和保存 todo： 1.label拖动和文字加载 2.文字水印加载至图片显示 3.文字样式控制 4.水印图片生成

--- emojigenerator/emojigenerator/Form1.Designer.cs
+++ emojigenerator/emojigenerator/Form1.Designer.cs
@@ -36,8 +36,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.Jpeg = new System.Windows.Forms.CheckBox();
+            this.Png = new System.Windows.Forms.CheckBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,6 +45,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Gif = new System.Windows.Forms.CheckBox();
+            this.Bmp = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -60,9 +62,9 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(590, 9);
+            this.groupBox1.Location = new System.Drawing.Point(436, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(182, 215);
+            this.groupBox1.Size = new System.Drawing.Size(182, 138);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "文字";
@@ -72,19 +74,21 @@
             this.groupBox2.Controls.Add(this.pictureBox1);
             this.groupBox2.Location = new System.Drawing.Point(12, 9);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(572, 540);
+            this.groupBox2.Size = new System.Drawing.Size(418, 332);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "效果";
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.checkBox2);
-            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.Bmp);
+            this.groupBox3.Controls.Add(this.Gif);
+            this.groupBox3.Controls.Add(this.Png);
+            this.groupBox3.Controls.Add(this.Jpeg);
             this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Location = new System.Drawing.Point(590, 230);
+            this.groupBox3.Location = new System.Drawing.Point(436, 156);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(182, 210);
+            this.groupBox3.Size = new System.Drawing.Size(182, 76);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "样式";
@@ -93,7 +97,7 @@
             // 
             this.groupBox4.Controls.Add(this.button2);
             this.groupBox4.Controls.Add(this.button1);
-            this.groupBox4.Location = new System.Drawing.Point(590, 446);
+            this.groupBox4.Location = new System.Drawing.Point(436, 238);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(182, 103);
             this.groupBox4.TabIndex = 2;
@@ -120,7 +124,7 @@
             // 
             // button1
             // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImage = global::emojigenerator.Properties.Resources.Delete;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.Location = new System.Drawing.Point(6, 20);
             this.button1.Name = "button1";
@@ -129,29 +133,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // checkBox1
+            // Jpeg
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(6, 32);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(78, 16);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.Jpeg.AutoSize = true;
+            this.Jpeg.Checked = true;
+            this.Jpeg.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.Jpeg.Location = new System.Drawing.Point(6, 32);
+            this.Jpeg.Name = "Jpeg";
+            this.Jpeg.Size = new System.Drawing.Size(48, 16);
+            this.Jpeg.TabIndex = 1;
+            this.Jpeg.Text = "Jpeg";
+            this.Jpeg.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // Png
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(6, 54);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(78, 16);
-            this.checkBox2.TabIndex = 2;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.Png.AutoSize = true;
+            this.Png.Location = new System.Drawing.Point(6, 54);
+            this.Png.Name = "Png";
+            this.Png.Size = new System.Drawing.Size(42, 16);
+            this.Png.TabIndex = 2;
+            this.Png.Text = "Png";
+            this.Png.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImage = global::emojigenerator.Properties.Resources.Save;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button2.Location = new System.Drawing.Point(98, 20);
             this.button2.Name = "button2";
@@ -162,7 +168,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(8, 44);
+            this.textBox1.Location = new System.Drawing.Point(8, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(168, 21);
             this.textBox1.TabIndex = 1;
@@ -170,7 +176,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 80);
+            this.label3.Location = new System.Drawing.Point(8, 56);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 2;
@@ -178,7 +184,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(8, 117);
+            this.textBox2.Location = new System.Drawing.Point(6, 71);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(168, 21);
             this.textBox2.TabIndex = 3;
@@ -186,7 +192,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 157);
+            this.label4.Location = new System.Drawing.Point(6, 95);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 4;
@@ -194,37 +200,61 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(10, 188);
+            this.textBox3.Location = new System.Drawing.Point(6, 110);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(166, 21);
             this.textBox3.TabIndex = 5;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(6, 17);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(560, 515);
+            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            // 
+            // Gif
+            // 
+            this.Gif.AutoSize = true;
+            this.Gif.Location = new System.Drawing.Point(90, 32);
+            this.Gif.Name = "Gif";
+            this.Gif.Size = new System.Drawing.Size(42, 16);
+            this.Gif.TabIndex = 3;
+            this.Gif.Text = "Gif";
+            this.Gif.UseVisualStyleBackColor = true;
+            // 
+            // Bmp
+            // 
+            this.Bmp.AutoSize = true;
+            this.Bmp.Location = new System.Drawing.Point(90, 54);
+            this.Bmp.Name = "Bmp";
+            this.Bmp.Size = new System.Drawing.Size(42, 16);
+            this.Bmp.TabIndex = 4;
+            this.Bmp.Text = "Bmp";
+            this.Bmp.UseVisualStyleBackColor = true;
             // 
             // UCCU
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(627, 353);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UCCU";
             this.ShowIcon = false;
-            this.Text = "Form1";
-            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.UCCU_DragDrop);
+            this.Text = "UCCU";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -248,12 +278,14 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox Png;
+        private System.Windows.Forms.CheckBox Jpeg;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox Bmp;
+        private System.Windows.Forms.CheckBox Gif;
     }
 }
 
--- emojigenerator/emojigenerator/Form1.cs
+++ emojigenerator/emojigenerator/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace emojigenerator
@@ -15,21 +10,115 @@ namespace emojigenerator
         public UCCU()
         {
             InitializeComponent();
+            this.pictureBox1.AllowDrop = true;
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBox1.DragDrop += new DragEventHandler(pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new DragEventHandler(pictureBox1_DragEnter);
         }
 
-        private void UCCU_DragDrop(object sender, DragEventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        /// <summary>
+        /// clear
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-
+            DisposeImage();
         }
+
+        /// <summary>
+        /// save
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                if (this.Jpeg.Checked) { bmp.Save(dir + "\\" + Guid.NewGuid().ToString().Replace("-", "").Substring(0,4) + ".jpg", ImageFormat.Jpeg); }
+                if (this.Gif.Checked) { bmp.Save(dir + "\\" + Guid.NewGuid().ToString().Replace("-", "").Substring(0,4) + ".gif", ImageFormat.Gif); }
+                if (this.Png.Checked) { bmp.Save(dir + "\\" + Guid.NewGuid().ToString().Replace("-", "").Substring(0,4) + ".png", ImageFormat.Png); }
+                if (this.Bmp.Checked) { bmp.Save(dir + "\\" + Guid.NewGuid().ToString().Replace("-", "").Substring(0, 4) + ".bmp", ImageFormat.Bmp); }
+            }
+        }
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                String[] strfileNames =
+                    (String[])e.Data.GetData(DataFormats.FileDrop);
+                Image dragImage = Image.FromFile(strfileNames[0]);
+                if (dragImage != null)
+                {
+                    this.DisposeImage();
+                    this.pictureBox1.Image = ZoomImage(dragImage);
+                }
+            }
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            bool bflag = e.Data.GetDataPresent(DataFormats.FileDrop);
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// 销毁
+        /// </summary>
+        public void DisposeImage()
+        {
+            if (this.pictureBox1.Image != null)
+            {
+                this.pictureBox1.Image.Dispose();
+                this.pictureBox1.Image = null;
+            }
+        }
+
+        /// <summary>
+        /// 缩放
+        /// </summary>
+        /// <param name="oldImage"></param>
+        /// <returns></returns>
+        private Image ZoomImage(Image oldImage)
+        {
+            if (null == oldImage)
+            {
+                return null;
+            }
+            int width = oldImage.Width;
+            int height = oldImage.Height;
+            int newWidth = 0;
+            int newHeight = 0;
+            float ratioXY = (float)width / (float)height;
+            if ((width > this.pictureBox1.Width) ||
+                 (height > this.pictureBox1.Height))
+            {
+                if (ratioXY >= 1.0)
+                {
+                    newWidth = this.pictureBox1.Width;
+                    newHeight = (int)((float)newWidth / ratioXY) + 1;
+                }
+                else
+                {
+                    newHeight = this.pictureBox1.Height;
+                    newWidth = (int)(newHeight * ratioXY);
+                }
+                Bitmap bitmap = new Bitmap(newWidth,
+                     newHeight, oldImage.PixelFormat);
+                Graphics g = Graphics.FromImage(bitmap);
+                g.Clear(Color.Transparent);
+                g.DrawImage(oldImage,
+                    new RectangleF(0, 0, newWidth, newHeight));
+                return Image.FromHbitmap(bitmap.GetHbitmap());
+            }
+            return oldImage;
+        }
+
+        #endregion Methods
     }
 }
